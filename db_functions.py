3051000c315e7d21d700b88edd665681678ebe57
Ousmane Seidy Diallo <ousmane.mudu@gmail.com>
2018-10-14 03:49:58 +0300

    refactor: move db connection to a function.

--- db_functions.py
+++ db_functions.py
@@ -1,13 +1,18 @@
 import os
 import sqlite3
 
+# Helper func to create db connection
+def db_connection():
+    conn = sqlite3.connect('posts.db')
+    cursor = conn.cursor()
+    return conn, cursor
+
 # Create the database and the Table
 def create_db():
     # if the db already exists delete it first
     if 'posts.db' in os.listdir():
         os.remove('posts.db')
-    conn = sqlite3.connect('posts.db')
-    cursor = conn.cursor()
+    conn, cursor = db_connection()
     with conn:
         # create Table
         cursor.execute('''
@@ -21,8 +26,7 @@ def create_db():
 # Save posts inside db table
 def save_posts(id, title, body):
     # connect to the db
-    conn = sqlite3.connect('posts.db')
-    cursor = conn.cursor()
+    conn, cursor = db_connection()
     with conn:
         # insert data
         cursor.execute('INSERT INTO posts VALUES (:id, :title, :body)',
@@ -30,14 +34,12 @@ def save_posts(id, title, body):
 
 # Get all posts
 def get_all_posts():
-    conn = sqlite3.connect('posts.db')
-    cursor = conn.cursor()
+    conn, cursor = db_connection()
     cursor.execute('SELECT * FROM posts')
     return cursor.fetchall()
 
 # Get post by id
 def get_post_by_id(postid):
-    conn = sqlite3.connect('posts.db')
-    cursor = conn.cursor()
+    conn, cursor = db_connection()
     cursor.execute('SELECT * FROM posts WHERE id=:id', {'id': postid})
     return cursor.fetchone()
